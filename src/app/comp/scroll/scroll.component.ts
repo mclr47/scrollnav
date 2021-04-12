46e821ec59b0e915b63cf0570a3b8352ffb64345
Martian Lapadatescu <arc@mwc-011240.dhcp.missouri.edu>
2021-04-11 21:45:41 -0500

    there is still lagging behind the fragment identification by the view and the window scroll and viewportScroll are not fully synchronous

--- src/app/comp/scroll/scroll.component.ts
+++ src/app/comp/scroll/scroll.component.ts
@@ -3,10 +3,13 @@ import { ActivatedRoute,Router } from '@angular/router';
 // import { FragmentService } from './../../service/fragment.service';
 import { Component, OnInit , ChangeDetectionStrategy,ChangeDetectorRef,
   ElementRef, ViewChild, HostListener, AfterViewInit } from '@angular/core';
-import {FixedSizeVirtualScrollStrategy, VIRTUAL_SCROLL_STRATEGY,
+import {FixedSizeVirtualScrollStrategy, VIRTUAL_SCROLL_STRATEGY,VirtualScrollStrategy,
+  
   ScrollDispatcher, CdkVirtualScrollViewport} from '@angular/cdk/scrolling';
 import {  Input, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import {Location, LocationStrategy, PathLocationStrategy, PlatformLocation,
+ } from '@angular/common';
 // 
 // export class CustomVirtualScrollStrategy extends FixedSizeVirtualScrollStrategy {
   // constructor() {
@@ -18,14 +21,20 @@ import { BehaviorSubject, Observable } from 'rxjs';
 //   }
 // }
 // 
+export interface HashExist {
+  hashVisible: boolean;
+  hashName?: string;
+}
 @Component({
   selector: 'app-scroll',
   templateUrl: './scroll.component.html',
   styleUrls: ['./scroll.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-  //  providers: [{provide: VIRTUAL_SCROLL_STRATEGY}]
-  //   //  useClass: CustomVirtualScrollStrategy
-  //   }]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+   providers: [{provide: VIRTUAL_SCROLL_STRATEGY,useClass:ScrollDispatcher},Location,
+     {provide: LocationStrategy, useClass: PathLocationStrategy}],
+     
+   
+// providers: [Location, {provide: LocationStrategy, useClass: ScrollComponent}],
 })
 export class ScrollComponent implements OnInit , AfterViewInit{
   // items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
@@ -34,18 +43,29 @@ export class ScrollComponent implements OnInit , AfterViewInit{
   start=0;
   scrollToPage = ['aaa','bbb','ccc','ddd','eee','fff','ggg','hhh','iii','jjj','kkk'] ;
   activeScrollIndex:Observable<number>;
-  activeFragment;
+  activatedRouteFragment = this.activatedRoute.fragment;
   activeFragmentSnapShot;
+  windowLocation;
   indexFocus;
-  activeFragment$: BehaviorSubject<string> = new BehaviorSubject(window.location.hash.valueOf().toString());
+  activeFragment$: BehaviorSubject<string> = new BehaviorSubject(window.location.hash.valueOf());
+  spyHash: HashExist= { hashVisible: false};   
+  spyHashCurrent: HashExist;
+
+  @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport; 
+  //  @HostListener('window:scroll',['$event']) onScrollEvent($event){
+  //   window.onhashchange (locationHashChanged)
+  //  }
 
   constructor(
     // private fragmentService:FragmentService,
-    private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2, 
+    @Inject(PLATFORM_ID) private platformId: Object,
     public activatedRoute:ActivatedRoute ,
     private router:Router,
     private ref: ChangeDetectorRef,
-    private scrollDispatcher:ScrollDispatcher) { 
+    private scrollDispatcher:ScrollDispatcher,
+    private platformLocation : PlatformLocation,
+    private location :Location) { 
       // this.activatedRoute.fragment.subscribe(
       //   (frag)=>{
       //     console.log(frag);
@@ -53,67 +73,153 @@ export class ScrollComponent implements OnInit , AfterViewInit{
       //   }
       // );
       // this.ref.detectChanges();
-      this.activatedRoute.snapshot.fragment.valueOf();
+      // this.activatedRoute.snapshot.fragment.valueOf();
+      // this.location.
+      // this.platformLocation.onHashChange(this.locationChangeListener)
+        // console.log(hash);
+       
+      
     }
 
   ngOnInit(): void {
-
-    this.activatedRoute.fragment.subscribe(
-      (frag)=>{
-        console.log(frag);
-        this.activeFragment = frag;
-      }
-    );
+    console.log('active fragment$$$$$ ' + this.activeFragment$.getValue())
+    // window.onload = this.isVisible();
+       // window.onhashchange = this.locationHashChanged;
+    // this.activatedRoute.fragment.subscribe(
+    //   (frag)=>{
+    //     console.log(frag);
+    //     this.activeFragment = frag;
+    //     const fragmentElement = document.querySelector('#'+frag);
+    //     if ( fragmentElement ){
+          
+    //         setTimeout(() => {
+    //         fragmentElement.scrollIntoView({behavior: 'smooth', block: 'start', 
+    //         inline: 'nearest'});
+    //         this.scroll(this.scrollToPage.indexOf(frag));
+    //         }, 0);
+    //     }
+    // this.renderer.listen(window,'onhashchange', (event)=>{
+    //      console.log('********* changing hash ******************')
+     
+    // window.onhashchange = this.funcOnHashChange;
+    // this.activatedRouteFragment.subscribe(
+    //   (frag)=>{
+    //     console.log(frag);
+    //     if(frag!=null && frag!=undefined)
+    //       {  console.log(this.scrollToPage.indexOf(frag));
+    //         // this.scroll(this.scrollToPage.indexOf(frag));}
+    //   } 
+    // }
+    // );
+  // this.renderer.listen(window, 'scroll', (event) => {
+    
+  //     // console.log (window.location.hash.valueOf().toString());
+  //     // this.windowLocation=window.location.hash.valueOf().toString()
+  //     this.scrollToPage.forEach( (val)=>{
+  //       console.log('value of renderer '+val);
+  //       console.log('#'+val);
+  //       console.log(this.scrollToPage.indexOf(val));
+  //     if (this.isVisible(document.querySelector('#'+val) as HTMLElement)) {
+  //       this.activeFragment$.next(val);
+  //       // this.activatedRoute.snapshot.fragment
+  //       this.scroll(this.scrollToPage.indexOf(val));}
+  //     }
+  //     )});
+    //   }
+    // );
+    // this.ref.detectChanges();
+  //   window.addEventListener("onhashchange", function () {
+  //     alert("Here");
+  // });
+  //  this.renderer.listen(window,'onhashchange', (event)=>{
+  //    console.log('********* changing hash ******************')
+  //  });
+//     this.renderer.listen(window,'scroll', (event) => {
+//       console.log('......renderer scrolled ....onInit');
+//       // if ( this.isVisible(document.querySelector('#iii')).hashVisible ===true){
+//       //   console.log('hash identified at iiiiiiiiii')
+//       // }
+//       // else {console.log('hash not found')}
+//       console.log(this.spyHash.hashName);
+// //  console.log(location.hash.valueOf());
+//     //  this.viewPort.scrollToIndex(
+//       //  this.scrollToPage.indexOf(    this.getCurrentHash().toString().substring(8) ))
+//       // this.ref.detectChanges();
+//       console.log(this.getCurrentHash());
+//         //   if (this.isVisible(document.querySelector('#ddd') as HTMLElement)) {
+//         // this.activeFragment$.next('ddd');}
+//     });
     // this.scrollToPage.includes
     // console.log (window.location.hash.valueOf.toString());
     // this.ref.detectChanges();
     this.renderer.listen(window, 'scroll', (event) => {
-      console.log (window.location.hash.valueOf().toString());
+      // console.log (window.location.hash.valueOf().toString());
+      // this.windowLocation=window.location.hash.valueOf().toString()
+      // this.scrollToPage.filter((val,index)=>{
+      //   if (this.isVisible(document.querySelector('#'+
+      //   this.activeFragment$.getValue()) as HTMLElement))
+
+      // })
+      console.log('Platform location .....&&&&& '+this.platformLocation.href);
+       this.location.subscribe((frag )=>
+      console.log(frag + 'this comes from location *****class constructor and service******'));
       if (this.isVisible(document.querySelector('#aaa') as HTMLElement)) {
         this.activeFragment$.next('aaa');
+        this.router.navigate(['./scroll'],{fragment:'aaa'});
         // this.activatedRoute.snapshot.fragment
         this.scroll(0);
       } else if (this.isVisible(document.querySelector('#bbb') as HTMLElement)) {
          this.activeFragment$.next('bbb');
+         this.router.navigate(['./scroll'],{fragment:'bbb'});
         this.scroll(1);
       } else if (this.isVisible(document.querySelector('#ccc') as HTMLElement)) {
         this.activeFragment$.next('ccc');
+        this.router.navigate(['./scroll'],{fragment:'ccc'});
         this.scroll(2);
       } 
       else if (this.isVisible(document.querySelector('#ddd') as HTMLElement)) {
          this.activeFragment$.next('ddd');
+         this.router.navigate(['./scroll'],{fragment:'ddd'});
         this.scroll(3);
       } 
       else if (this.isVisible(document.querySelector('#eee') as HTMLElement)) {
          this.activeFragment$.next('eee');
+         this.router.navigate(['./scroll'],{fragment:'eee'});
         this.scroll(4);
       } 
       else if (this.isVisible(document.querySelector('#fff') as HTMLElement)) {
         this.activeFragment$.next('fff');
+        this.router.navigate(['./scroll'],{fragment:'fff'});
         this.scroll(5);
       } 
       else if (this.isVisible(document.querySelector('#ggg') as HTMLElement)) {
          this.activeFragment$.next('ggg');
+         this.router.navigate(['./scroll'],{fragment:'ggg'});
         this.scroll(6);
       } 
       else if (this.isVisible(document.querySelector('#hhh') as HTMLElement)) {
          this.activeFragment$.next('hhh');
+         this.router.navigate(['./scroll'],{fragment:'hhh'});
         this.scroll(7);
       } 
       else if (this.isVisible(document.querySelector('#iii') as HTMLElement)) {
          this.activeFragment$.next('iii');
+         this.router.navigate(['./scroll'],{fragment:'iii'});
         this.scroll(8);
       } 
       else if (this.isVisible(document.querySelector('#jjj') as HTMLElement)) {
         this.activeFragment$.next('jjj');
+        this.router.navigate(['./scroll'],{fragment:'jjj'});
+        console.log(this.isVisible(document.querySelector('#jjj') as HTMLElement))
         this.scroll(9);
       } 
       else if (this.isVisible(document.querySelector('#kkk') as HTMLElement)) {
          this.activeFragment$.next('kkk');
+         this.router.navigate(['./scroll'],{fragment:'kkk'});
         this.scroll(10);
       } 
     
-       this.ref.detectChanges();
+      //  this.ref.detectChanges();
     
     });
 
@@ -185,30 +291,64 @@ export class ScrollComponent implements OnInit , AfterViewInit{
           
         });
        
-    this.ref.detectChanges();
+    // this.ref.detectChanges();
     this.scrollDispatcher.scrolled()
     .subscribe(event => {
       console.log('scrolled........');
+       console.log(this.getCurrentHash())
+      console.log(this.getCurrentHash())
     });
+    // this.scrollDispatcher.ancestorScrolled().subscribe()
+   
   }
 // *******************
 
   currentElement: string; 
 
-  @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport; 
+  // @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport; 
   // @HostListener('window:scroll',['$event']) onScrollEvent($event){
   //   window.onhashchange = this.locationHashChanged;
   // }
   locationHashChanged(){
+
+    // this.activatedRoute.fragment.subscribe(
+    //   (frag)=>{
+    //     console.log(frag);
+    //     this.activeFragment = frag;
+    //     const fragmentElement = document.querySelector('#'+frag);
+    //     if ( fragmentElement ){
+          
+    //         setTimeout(() => {
+    //         fragmentElement.scrollIntoView({behavior: 'smooth', block: 'start', 
+    //         inline: 'nearest'});
+    //         this.scroll(this.scrollToPage.indexOf(frag));
+    //         }, 0);
+    //     }
+
+    //   }
+    // );
+    // this.scroll(this.scrollToPage.indexOf(location.hash));
+    // console.log(this.scrollToPage.indexOf(location.hash));
  
   }
   
-
+  funcOnHashChange(){
+    console.log('###### changing the hash ##########')
+  }
    updateVerticalScroll(event): void {
  
   }
+ getCurrentHash() {
+    return decodeURIComponent(this.platformLocation.hash.replace(/^#\/scroll#/, ''));
+    // this.platformLocation.onHashChange(hash=>{
+    //   console.log(hash);
+    // })
+  
+  }
 
- 
+ locationChangeListener(){
+   console.log(this.activatedRoute.fragment)
+ }
   scroll(position) {
    
     // this.viewPort.scrollToIndex(position, 'smooth');
@@ -216,6 +356,10 @@ export class ScrollComponent implements OnInit , AfterViewInit{
   }
 
   isVisible(elem: HTMLElement) {
+  //  
+  
+  // 
+
     if (!(elem instanceof Element)) {
       throw Error('DomUtil: elem is not an element.');
     }
@@ -223,14 +367,18 @@ export class ScrollComponent implements OnInit , AfterViewInit{
     const style = getComputedStyle(elem);
 
     if (style.display === 'none') {
-      return false;
+      // this.spyHash.hashVisible = false;
+       return false;
+      
     }
 
     if (style.visibility !== 'visible') {
-      return false;
+      this.spyHash.hashVisible = false;
+      // return false;
     }
 
     if (+style.opacity < 0.1) {
+      // this.spyHash.hashVisible = false;
       return false;
     }
 
@@ -241,7 +389,8 @@ export class ScrollComponent implements OnInit , AfterViewInit{
         elem.getBoundingClientRect().width ===
       0
     ) {
-      return false;
+      // this.spyHash.hashVisible = false;
+       return false;
     }
 
     const elemCenter = {
@@ -250,29 +399,41 @@ export class ScrollComponent implements OnInit , AfterViewInit{
     };
 
     if (elemCenter.x < 0) {
-      return false;
+       return false;
+      // this.spyHash.hashVisible = false;
     }
 
     if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) {
-      return false;
+       return false;
+      // this.spyHash.hashVisible = false;
     }
 
     if (elemCenter.y < 0) {
-      return false;
+       return false;
+      // this.spyHash.hashVisible = false;
     }
 
     if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) {
       return false;
+      // this.spyHash.hashVisible = false;
     }
 
     let pointContainer: any = document.elementFromPoint(elemCenter.x, elemCenter.y);
     do {
       if (pointContainer === elem) {
+        console.log(elem.id);
+        this.spyHash.hashVisible = true;
+        this.spyHash.hashName = elem.id;
+        this.activeFragment$.next(elem.id);
         return true;
+        // return this.spyHash;
       }
-    } while ((pointContainer = pointContainer.parentNode));
+      // return this.spyHash;
+    } 
+     while ((pointContainer = pointContainer.parentNode));
 
-    return false;
+     return false;
+    // return this.spyHash;
   }
 
 }
